0a320f69a5c909f05f28b5960f14ed080ece0dc5
JustSailing <136002471+JustSailing@users.noreply.github.com>
2023-10-04 15:05:54 -0400

    combined comp vert and frag into on class.

--- Src/shader.cpp
+++ Src/shader.cpp
@@ -1,44 +1,74 @@
 // System
 #include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <filesystem>
 #include <fstream>
+#include <iostream>
 // User-Defined
+#include "colorIO.hpp"
 #include "shaders.hpp"
 
 namespace mini_engine {
-//Public
+// Public
 Shader_Comp::Shader_Comp(const path &path_to_shaderc, const path &path_to_input,
                          const path &path_to_output)
     : m_shaderc{path_to_shaderc}, m_in_dir{path_to_input},
       m_out_dir{path_to_output} {
-        m_vert_count = compile_vert();
-        m_frag_count = compile_frag();
+  // TODO: check if directory exist
+  compile_shader();
 }
 
-auto Shader_Comp::vert_compile_amt() const -> const int {
-    return m_vert_count;
-}
-auto Shader_Comp::frag_compile_amt() const -> const int {
-    return m_frag_count;
-}
+auto Shader_Comp::vert_compile_amt() const -> const int { return m_vert_count; }
+auto Shader_Comp::frag_compile_amt() const -> const int { return m_frag_count; }
 
 // Static
 auto Shader_Comp::read_file(const path &p) -> vector<char> {
-    std::ifstream file(p, std::ios::ate | std::ios::binary);
-    if (file.fail()) {
-        fprintf(stderr, "Shader Compilation: file in read_file failure");
-        exit(2);
-    }
-    auto file_size = static_cast<size_t>(file.tellg());
-    vector<char> data(file_size);
-    file.seekg(0);
-    file.read(data.data(), file_size);
-    file.close();
-    return data;
+  std::ifstream file(p, std::ios::ate | std::ios::binary);
+  if (file.fail()) {
+    fprintf(stderr, "%s file in read_file failure\n", CRED("Shader Compilation: "));
+    exit(2);
+  }
+  auto file_size = static_cast<size_t>(file.tellg());
+  vector<char> data(file_size);
+  file.seekg(0);
+  file.read(data.data(), file_size);
+  file.close();
+  return data;
 }
 
 // Private
-auto Shader_Comp::compile_vert() -> int {
-    
+auto Shader_Comp::compile_shader() -> void {
+  using recursive_dir_iter = std::filesystem::recursive_directory_iterator;
+  for (auto const &file : recursive_dir_iter{m_in_dir}) {
+    // std::cout << file.path().extension().string() << '\n'; // test
+    if (file.path().extension().string() == ".vert") {
+      std::string exe = m_shaderc.string() + " " + file.path().string() +
+                        " -o " + m_out_dir.string() + "/" +
+                        file.path().filename().string() + ".spv";
+      if (std::system(static_cast<const char *>(exe.c_str())) != 0) {
+        fprintf(stderr, "Shader: failed to compile %s\n", file.path().c_str());
+        continue;
+      }
+      std::cout << CGRN("Shader: ") << exe << " was executed\n";
+      ++m_vert_count;
+    } else if (file.path().extension().string() == ".frag") {
+      std::string exe = m_shaderc.string() + " " + file.path().string() +
+                        " -o " + m_out_dir.string() + "/" +
+                        file.path().filename().string() + ".spv";
+      if (std::system(static_cast<const char *>(exe.c_str())) != 0) {
+        fprintf(stderr, "Shader: failed to compile %s\n",
+                file.path().string().c_str());
+        continue;
+      }
+      std::cout << CGRN("Shader: ") << exe << " was executed\n";
+      ++m_frag_count;
+    } else {
+      fprintf(stderr, "%s: Shader: file not a vert or frag file: %s\n", CYEL("Warning"),
+              file.path().c_str());
+      continue;
+    }
+  }
 }
 
 } // namespace mini_engine
--- Src/shaders.hpp
+++ Src/shaders.hpp
@@ -4,28 +4,36 @@
 #include <vector>
 
 namespace mini_engine {
-using std::filesystem::path;
 using std::vector;
-
+using std::filesystem::path;
+// NOTE: This is more of a utility class
 class Shader_Comp {
 public:
-  explicit Shader_Comp(const path &path_to_shaderc, const path &path_to_input, const path &path_to_output);
+  explicit Shader_Comp(const path &path_to_shaderc, const path &path_to_input,
+                       const path &path_to_output);
   auto inline vert_compile_amt() const -> const int;
   auto inline frag_compile_amt() const -> const int;
 
 private:
   /// Member functions
+
   // function below should use the paths provided in constructor to compile all
   // vert and frag shaders into spir-v files and put them in output directory
-  auto compile_vert() -> int;
-  auto compile_frag() -> int;
-  // called to for vkCreateShaderModule
+  auto compile_shader() -> void; // count saved in member variables
+
+  // TODO: Should add overloaded function in order compile specific shaders
+
+  //  called to for vkCreateShaderModule
   static auto read_file(const path &p) -> vector<char>;
+
   /// Member Variables
   int m_vert_count{0};
   int m_frag_count{0};
   path m_in_dir;
   path m_out_dir;
   path m_shaderc;
+  // NOTE: Should probably add booleans to check if directorys are valid as well
+  // as if shader compiler is valid if we want to change compilers or I/O
+  // directories
 };
 } // namespace mini_engine
