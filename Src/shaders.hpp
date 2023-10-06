a734faf47d1f1d722449b259d4adb22aaa393eff
JustSailing <136002471+JustSailing@users.noreply.github.com>
2023-10-06 17:42:13 -0400

    made read_file public; added no discard

--- Src/shaders.hpp
+++ Src/shaders.hpp
@@ -11,21 +11,26 @@ class Shader_Comp {
 public:
   explicit Shader_Comp(const path &path_to_shaderc, const path &path_to_input,
                        const path &path_to_output);
+
+  [[nodiscard("returns the amount of files with .vert that were compiled")]]
   auto inline vert_compile_amt() const -> const int;
+
+  [[nodiscard("returns the amount of files with .frag that were compiled")]]
   auto inline frag_compile_amt() const -> const int;
+
+  // TODO: rather than exiting should return an error. Possibly add a parameter that will
+  // fit both an enum type that will give success or some error
+  [[nodiscard("returns a vector of chars of the shader file requested")]]
+  static auto read_file(const path &p) -> vector<char>;
+  // TODO: Should add overloaded function in order compile specific shaders
 
 private:
   /// Member functions
 
-  // function below should use the paths provided in constructor to compile all
+  // function should use the paths provided in constructor to compile all
   // vert and frag shaders into spir-v files and put them in output directory
   auto compile_shader() -> void; // count saved in member variables
-
-  // TODO: Should add overloaded function in order compile specific shaders
-
-  //  called to for vkCreateShaderModule
-  static auto read_file(const path &p) -> vector<char>;
-
+  
   /// Member Variables
   int m_vert_count{0};
   int m_frag_count{0};
