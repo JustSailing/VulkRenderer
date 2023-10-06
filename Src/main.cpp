d5e32087b027858fcdb6203d6bdd39141db3e5bf
JustSailing <136002471+JustSailing@users.noreply.github.com>
2023-10-06 11:57:37 -0400

    changed to see if X11 worked properly

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,37 +1,42 @@
-#include "glm/ext/matrix_float4x4.hpp"
-#include "glm/ext/vector_float4.hpp"
+#include "colorIO.hpp"
 #include "shaders.hpp"
-#include "vulkan/vulkan_core.h"
-#include <iostream>
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-#include <vulkan/vulkan.hpp>
-#include <glm/mat4x4.hpp>
 #include "window.hpp"
+#include <X11/X.h>
+#include <X11/Xlib.h>
+#include <cstdio>
+#include <glm/mat4x4.hpp>
+#include <iostream>
+#include <vulkan/vulkan.hpp>
 
-auto main(int argc, char** argv) -> int {
-    std::cout << "Hello World" << '\n';
-    if (!glfwInit()) {
-        std::cout << "glfw failed to initialize" << '\n';
-        glfwTerminate();
-        return 1;
+auto main(int argc, char **argv) -> int {
+  mini_engine::Window_T window(800, 600, "Vulkan Window");
+  if (!window.set_attributes(KeyPressMask | KeyReleaseMask |
+                             StructureNotifyMask))
+    std::cout << "setting attributes for X11 window failed\n";
+  uint32_t extensionCount = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+  std::cout << extensionCount << " Extensions supported\n";
+
+  glm::mat4 matrix;
+  glm::vec4 vec;
+  auto test = matrix * vec;
+  mini_engine::Shader_Comp comp{"../Vendor/Executable/glslc", "../Shader/Input",
+                                "../Shader/Output"};
+
+  while (1) {
+    std::optional<XEvent> event = window.get_event();
+    if (event.has_value()) {
+      if (event->type == ClientMessage) {
+        if (window.check_destroy_win(*event)) {
+          fprintf(stderr, "%sDestroyed by WM\n", CGRN("WINDOW: "));
+          break;
+        }
+      }
+      fprintf(stderr, "%s%d\nKeyPressEvent type %d\n", CGRN("EVENT_Type: "),
+              event->type, event->xkey.keycode);
     }
-    
+  }
 
-    mini_engine::Window window(800, 600, "Vulkan Window", true, nullptr, nullptr);
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " Extensions supported\n";
-    
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-    mini_engine::Shader_Comp comp{"../Vendor/Executable/glslc","../Shader/Input", "../Shader/Output"};
-
-    while (!glfwWindowShouldClose(window.get_window())) {
-        glfwPollEvents();
-    }
-
-    return 0;
+  return 0;
 }
