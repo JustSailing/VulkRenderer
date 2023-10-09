718c880596f0d882c49e6d307dbd8d08677bec1c
JustSailing <136002471+JustSailing@users.noreply.github.com>
2023-10-09 11:55:45 -0400

    formatting and added dynamic loader to vulkan dev

--- Src/device.hpp
+++ Src/device.hpp
@@ -1,5 +1,5 @@
-#include <vector>
 #include <string>
+#include <vector>
 
 #include <vulkan/vulkan.hpp>
 
@@ -8,22 +8,24 @@ class Vulkan_Device {
 public:
   explicit Vulkan_Device(std::string app_name, bool debug);
   ~Vulkan_Device();
-  
-private:
-/// Member functions
-auto create_instance(std::string app_name) -> void;
 
-/// Member variables
-vk::Instance m_vk_instance{};
-// supported vulkan api version
-uint32_t m_version;
-// supported extension properties
-std::vector<vk::ExtensionProperties> m_ext_props;
-// supported layer properties
-std::vector<vk::LayerProperties> m_layer_props;
-// debugger
-vk::DebugUtilsMessengerEXT m_debugger;
-// physical devices
-std::vector<vk::PhysicalDevice> m_physical_devs;
+private:
+  /// Member functions
+  auto create_instance(std::string app_name) -> void;
+
+  /// Member variables
+  vk::Instance m_vk_instance{};
+  // supported vulkan api version
+  uint32_t m_version;
+  // supported extension properties
+  std::vector<vk::ExtensionProperties> m_ext_props;
+  // supported layer properties
+  std::vector<vk::LayerProperties> m_layer_props;
+  // dynamic loader to run function not present statically
+  vk::DispatchLoaderDynamic m_dyn_loader;
+  // debugger
+  vk::DebugUtilsMessengerEXT m_debugger;
+  // physical devices
+  std::vector<vk::PhysicalDevice> m_physical_devs;
 };
 } // namespace mini_engine
